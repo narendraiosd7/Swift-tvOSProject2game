f5d2e4d84313db7886e7a927ee7bf0306e080324
Narendra <narendra.vadde@zee.esselgroup.com>
2020-10-12 13:47:30 +0530

    Initial Commit

---
+++ Swift-tvOSProject2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift-tvOSProject2
+//
+//  Created by narendra. vadde on 12/10/20.
+//  Copyright © 2020 narendra. vadde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
